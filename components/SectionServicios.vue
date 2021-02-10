3a01d658833268a5a9a4a76b9adb9d2290b83e83
SirRobert-1 <rviverosgonzalez@gmail.com>
2021-02-09 22:58:14 -0600

    puse la informacion de las  biografias y de los servicios en sus apartados

--- components/SectionServicios.vue
+++ components/SectionServicios.vue
@@ -2,22 +2,39 @@
     <section class="Servicios bg-black" id="servicios">
         <h1 class="text-white">/Servicios</h1>
         <div class="flex flex-row justify-between">
-            <h1 class="Sercicios__title text-white">PONER UNA FRASE O COPY BIEN CHIDORI CHIDORIa</h1>
+            <h1 class="Servicios__title text-white">PONER UNA FRASE O COPY BIEN CHIDORI CHIDORIa</h1>
             <div class="Servicios__list text-white flex flex-row">
                 <ul>
-                    <li>Servicio</li>
-                    <li>Servicio</li>
-                    <li>Servicio</li>
-                    <li>Servicio</li>
-                    <li>Servicio</li>
+                    <li>Largometrajes</li>
+                    <li>Cortometrajes</li>
+                    <li>Documentales</li>
+                    <li>Comerciales</li>
+                    <li>Fashion films</li>
+                    <li>VFX</li>
+                    <li>Series</li>
+                    <li>Video clips</li>
+                    <li>Corporativo</li>
+                    <li>Institucional</li>
+                    <li>Eventos especiales</li>
                 </ul>
 
                 <ul>
-                    <li>Servicio</li>
-                    <li>Servicio</li>
-                    <li>Servicio</li>
-                    <li>Servicio</li>
-                    <li>Servicio</li>
+                    <li>Social</li>
+                    <li>Fashion</li>
+                    <li>Artistica</li>
+                    <li>Producto</li>
+                    <li>Publicitaria</li>
+                    <li>Documental</li>
+                </ul>
+                <ul>
+                    <li>Mezcla</li>
+                    <li>Mastering</li>
+                    <li>Grabacion de discos</li>
+                    <li>Arreglos musicales y grabaciones con musicos de sesión</li>
+                    <li>Diseño sonoro</li>
+                    <li>Sonido directo</li>
+                    <li>Jingles para radio y TV</li>
+                    <li>Montaje musical para promociones</li>
                 </ul>
             </div>
         </div>
